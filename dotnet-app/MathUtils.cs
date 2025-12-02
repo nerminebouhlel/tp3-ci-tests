8484125506492e23f8a48f9dab8903125a074388
Nermine Bouhlel <nerminebouhlel17@gmail.com>
2025-12-02 13:55:11 +0100

    Initial commit - ajout projet TP3

---
+++ dotnet-app/MathUtils.cs
@@ -0,0 +1,8 @@
+public static class MathUtils
+{
+public static int Factorial(int n)
+{
+if (n < 0) throw new ArgumentException("n must be non-negative");
+return n <= 1 ? 1 : n * Factorial(n - 1);
+}
+}
